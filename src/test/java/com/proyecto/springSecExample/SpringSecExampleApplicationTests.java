42993d23b9e9e3360e35ade8e3ab3336b8e36170
christianorlando <cogv1981@gmail.com>
2021-12-17 10:25:24 +0100

    first commit

---
+++ src/test/java/com/proyecto/springSecExample/SpringSecExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.springSecExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class SpringSecExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
